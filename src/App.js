cf707c78f274c25e13884f4a892860b8eff1614a
unknown <theziaur@gmail.com>
2023-01-30 11:35:01 +0530

    new page has been added

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import ImageProcessing from './components/ImageProcessing'
+function App() {
+  return (
+    <div className="App">
+      <ImageProcessing />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ImageProcessing.js
@@ -0,0 +1,10 @@
+import React from "react"
+class ImageProcessing extends React.Component {
+    render() {
+        return (
+            <h1>Image Processing</h1>
+        )
+    }
+}
+
+export default ImageProcessing
